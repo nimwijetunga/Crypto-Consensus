09c3d6afa5433b5ea301984eef811c92bcd25b40
vnwijetu <vnwijetu@uwaterloo.ca>
2018-02-24 18:36:41 -0500

    Finished Backend Service

--- backend/text_analytics.js
+++ backend/text_analytics.js
@@ -8,21 +8,10 @@ var fs = require('fs');
 
 var obj;
 
-// **********************************************
-// *** Update or verify the following values. ***
-// **********************************************
 
-// Replace the accessKey string value with your valid access key.
 let accessKey = '6e8dd270bf5941129c8b06a1627207c6';
 
-// Replace or verify the region.
 
-// You must use the same region in your REST API call as you used to obtain your access keys.
-// For example, if you obtained your access keys from the westus region, replace 
-// "westcentralus" in the URI below with "westus".
-
-// NOTE: Free trial access keys are generated in the westcentralus region, so if you are using
-// a free trial access key, you should not need to change this region.
 let uri = 'eastus.api.cognitive.microsoft.com';
 let path = '/text/analytics/v2.0/sentiment';
 
